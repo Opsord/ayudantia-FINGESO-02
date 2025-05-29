573f569b2a5ac3fde7242f2e44c994a19c1fd6a6
Andres Zelaya <andres.zelaya.d@hotmail.com>
2025-05-29 08:33:19 -0400

    Project init

---
+++ AyudantiaFINGESO/src/main/java/com/example/AyudantiaFINGESO/AyudantiaFingesoApplication.java
@@ -0,0 +1,13 @@
+package com.example.AyudantiaFINGESO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyudantiaFingesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyudantiaFingesoApplication.class, args);
+	}
+
+}
---
+++ AyudantiaFINGESO/src/test/java/com/example/AyudantiaFINGESO/AyudantiaFingesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AyudantiaFINGESO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyudantiaFingesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
